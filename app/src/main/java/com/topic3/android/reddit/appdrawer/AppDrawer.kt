574174cbb2a8ad7242db067d0e8afd5d1e78b4b0
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 20:49:39 +0300

    Changed function AppDrawerBody

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -10,7 +10,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
@@ -188,6 +190,22 @@ private fun ProfileInfoItem(
 @Composable
 private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
   //TODO add your code here
+  Column {
+    ScreenNavigationButton(
+      icon = Icons.Filled.AccountBox,
+      label = stringResource(R.string.my_profile),
+      onClickAction = {
+        closeDrawerAction()
+      }
+    )
+    ScreenNavigationButton(
+      icon = Icons.Filled.Home,
+      label = stringResource(R.string.saved),
+      onClickAction = {
+        closeDrawerAction()
+      }
+    )
+  }
 }
 
 /**
